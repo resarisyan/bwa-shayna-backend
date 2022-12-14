ee8687d41117ff2f460561172c17841ab06348dd
resarisyan <resarisyan77@gmail.com>
2022-12-15 06:18:25 +0700

    initial commit

---
+++ app/Models/Transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [
+        'uuid', 'name', 'email', 'number', 'address',
+        'transaction_total', 'transaction_status'
+    ];
+    protected $hidden = [];
+
+    public function details()
+    {
+        return $this->hasMany(TransactionDetail::class, 'transactions_id');
+    }
+}
---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [
+        'products_id', 'transactions_id'
+    ];
+    protected $hidden = [];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+}
